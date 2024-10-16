c59f7ba8acaf8b22812ecfd88d9a65f7004d73be
DangQuangKy <quangky250896@gmail.com>
2024-10-16 10:19:37 +0700

    all code

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Logout from "./pages/logout/logout";
+
+function App() {
+  return (
+    <div className="app">
+      <Logout />
+    </div>
+  );
+}
+
+export default App;
